baba47ced6f8f1e90a0f4e3122ae30242344cebe
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-04-22 03:09:05 -0300

    Feat: Requisitos 20 e 21 - Realizei pequenas mudanças nos buttons do Feedback e Ranking, para realizar a cobertura de testes

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { resetScore } from '../redux/actions/gameAction';
 
 class Ranking extends Component {
   state = {
